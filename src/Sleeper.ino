0392b672c176bad18b1262e5f7b81283abaf5e86
niksan004 <57328380+niksan004@users.noreply.github.com>
2019-11-15 23:29:30 -0800

    Add files via upload

--- src/Sleeper.ino
+++ src/Sleeper.ino
@@ -2,15 +2,14 @@
 #include "RTClib.h"
 
 const int chipSelect = 10;
-const int PIR = 2;
-const int MIC = A4;
-const int number_of_readings = 600;
-const int func_delay = 100;
-char file_name[32];
-char data_in_file[32];
+const int PIR = 2;                  //Инициализация на пин за сензора за движение
+const int MIC = A4;                 //Инициализация на пин за микрофона
+const int number_of_readings = 60;  //Променливите за четенията и забавянето са нагласени така,
+const int func_delay = 100;         //че по-късно функцията да трае 1 минута
+char file_name[32];                 //Инициализация на името на файла
+char data_in_file[32];              //Инициализация на датата във файла
 
-int movements_per_min();
-int sound_per_min();
+void movements_and_sound_per_min(); //Инициализация на функцията за четене на сензорите
 
 RTC_DS1307 RTC;
 File dataFile;
@@ -32,12 +31,12 @@ void setup(void) {
   Serial.println("initialised");  
   
   now = RTC.now();  
-  sprintf(file_name, "%02d%02d%02d%02d.txt", now.month(), now.day(), now.hour(), now.minute());
+  sprintf(file_name, "%02d%02d%02d%02d.txt", now.month(), now.day(), now.hour(), now.minute()); //Създаване на името на файла
 }
 
 void loop() {
   now = RTC.now();  
-  sprintf(data_in_file, "%4d/%02d/%02d %02d:%02d", now.year(), now.month(), now.day(), now.hour(), now.minute());
+  sprintf(data_in_file, "%4d/%02d/%02d %02d:%02d", now.year(), now.month(), now.day(), now.hour(), now.minute()); //Създаване на датата във файла
 
   dataFile = SD.open(file_name, FILE_WRITE);
   dataFile.print(data_in_file);
@@ -47,39 +46,41 @@ void loop() {
 }
 
 void movements_and_sound_per_min(){
-  //Movement Variables
+  //Променливи за движението
   int present_reading_movement = 0;
   float sum_of_readings_movement = 0;
 
-  //Sound Variables
+  //Променливи за звука
   int present_reading_sound = 0;
   int past_reading_sound = 0;
   float sum_of_readings_sound = 0;
   int max_sound = 0;
+  int diviation = 0;
 
   for(int i=0; i<number_of_readings; i++){
-    present_reading_movement = digitalRead(PIR);
-    sum_of_readings_movement += present_reading_movement;
-    //Serial.println(present_reading_movement);
+   //Отчитане на движение
+   present_reading_movement = digitalRead(PIR);          
+   sum_of_readings_movement += present_reading_movement;
 
-    present_reading_sound = analogRead(MIC);
-    if(past_reading_sound == 0){ past_reading_sound = present_reading_sound; }
+   //Отчитане на звук
+   present_reading_sound = analogRead(MIC);
+   if(past_reading_sound == 0) past_reading_sound = present_reading_sound; 
+
+   diviation = abs(present_reading_sound - past_reading_sound);
+   sum_of_readings_sound += diviation;
     
-    sum_of_readings_sound += abs(present_reading_sound - past_reading_sound);
+   if(max_sound == 0 || diviation > max_sound) max_sound = diviation; 
     
-    if(max_sound == 0 || abs(present_reading_sound - past_reading_sound) > max_sound){ 
-      max_sound = abs(present_reading_sound - past_reading_sound); 
-    }
     past_reading_sound = present_reading_sound;
     delay(func_delay);  
   }
   
-  /*dataFile.print((int)(sum_of_readings_movement / number_of_readings * 100));
-  dataFile.print(", ");
-  dataFile.println((int)(sum_of_readings_sound / number_of_readings));*/
+  //dataFile.print((int)(sum_of_readings_movement / number_of_readings * 100));
+  //dataFile.print(", ");
+  //dataFile.print((int)(sum_of_readings_sound / number_of_readings));
   Serial.print((int)(sum_of_readings_movement / number_of_readings * 100));
   Serial.print(", ");
   Serial.print((int)(sum_of_readings_sound / number_of_readings));
-  Serial.print(", ");
-  Serial.println(max_sound);
+  //dataFile.print(", ");
+  //dataFile.println(max_sound);
 }
